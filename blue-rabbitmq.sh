7e4da3ac1b6a5f840539cf98aa9842421cdfa191
huanletuyen <44225534+huanletuyen@users.noreply.github.com>
2024-10-07 12:33:45 +0700

    Update blue-rabbitmq.sh

--- blue-rabbitmq.sh
+++ blue-rabbitmq.sh
@@ -3,7 +3,7 @@
 # Kiểm tra số lượng đối số
 if [ "$#" -eq 0 ]; then
     echo "No arguments provided. Please provide the necessary arguments."
-    echo "Usage: ./blue-rabbitmq.sh --export|--import-data --number-queue=QUEUE_NAME --host=HOST --username=USERNAME --password=PASSWORD --input-file=INPUT_FILE --output-file=OUTPUT_FILE"
+    echo "Usage: ./blue-rabbitmq.sh --export-data|--import-data --queue=QUEUE_NAME --host=HOST --username=USERNAME --password=PASSWORD --input-file=INPUT_FILE --output-file=OUTPUT_FILE"
     exit 1
 fi
 
